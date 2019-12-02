a98a2705628e0a4adfc2e57956993ea80d875f04
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-02 15:14:52 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$km = 1;
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+   <head>
+     <meta charset="utf-8" />
+     <title>PHP Partie1 exo3</title>
+   </head>
+   <body>
+     <p><?php echo $km; ?></p>
+     <p><?php echo $km + 2; ?></p>
+     <p><?php echo $km + 124; ?></p>
+   </body>
+ </html>
