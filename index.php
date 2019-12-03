ee18e42e3a8a4c09c0541022d967130fd3534bb6
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 13:20:11 +0100

    correction

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
+//déclaration d'une variable 'km' initialisée par '1'
 $km = 1;
- ?>
- <!DOCTYPE html>
- <html lang="fr" dir="ltr">
-   <head>
-     <meta charset="utf-8" />
-     <title>PHP Partie1 exo3</title>
-   </head>
-   <body>
-     <p><?php echo $km; ?></p>
-     <p><?php echo $km + 2; ?></p>
-     <p><?php echo $km + 124; ?></p>
-   </body>
- </html>
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>PHP Partie1 exo3</title>
+</head>
+<body>
+  <p><?php echo $km; ?></p>
+  <p><?php echo $km + 2; ?></p>
+  <p><?php echo $km + 124; ?></p>
+</body>
+</html>
